0853230501717f0653a4285bba9ed0b0bcc0bf29
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-08 13:39:04 -0500

    More styling changes

--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -5,14 +5,16 @@ import styled from 'styled-components';
 const Card = styled.div`
     width: 300px;
     max-height: 30rem;
-    background: snow;
+    background: rgb(2,0,36);
+background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(255,255,255,1) 0%, rgba(207,224,227,1) 100%);
     margin: 20px;
     border-radius: 10px;
     font-family: 'Raleway', sans-serif;
 `;
 
 const Name = styled.h2`
-    background: slategrey;
+background: rgb(171,171,171);
+background: linear-gradient(0deg, rgba(171,171,171,1) 0%, rgba(79,79,79,1) 100%);
     width: 100%;
     height: 70px;
     display:flex;
@@ -20,6 +22,7 @@ const Name = styled.h2`
     justify-content: center;
     margin: 0px;
     border-radius: 10px 10px 0px 0px;
+    color: white;
     
 `;
 
